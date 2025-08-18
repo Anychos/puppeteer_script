980cba42abecfa7089acd87f0a985192cc3430c9
Anychnyi_Anton <anychnyianton@gmail.com>
2025-08-18 11:18:07 +0400

    добавлены новые проверки со скринами на другие экраны

--- test_4k_item_page.js
+++ test_4k_item_page.js
@@ -18,18 +18,49 @@ const puppeteer = require('puppeteer');  // Импорт Puppeteer
     });
 
     const page = await browser.newPage();  // Запуск браузера
-    await page.goto('https://a101-shop-front-merch-4k.ktsdev.ru/?platform=stand', { waitUntil: 'networkidle2' });  // Открытие сайта с параметром ожидания
+    await page.goto('https://a101-shop-front-specstorea-355.ktsdev.ru/?platform=stand', { waitUntil: 'networkidle2' });  // Открытие сайта с параметром ожидания
 
-    await new Promise(resolve => setTimeout(resolve, 3000));  // Ожидание 3 секунды
+    await new Promise(resolve => setTimeout(resolve, 5000));  // Ожидание 3 секунды
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_catalog.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
 
     // Клик по полю ввода. Используется CSS селектор
-    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.X3RIOxB4Hfw\\=.hnvkvWHmDgE\\= > div.rI9Jk8q43D8\\= > div > div > div:nth-child(1) > div > article:nth-child(3) > a');
+    await page.click('#main-scroll > div > div.rI9Jk8q43D8\\= > div > div > div:nth-child(1) > div > article:nth-child(1)');
 
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 5000));
     // Скриншот
-    await page.screenshot({ path: 'delayed-screenshot.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+    await page.screenshot({ path: 'delayed-screenshot_product_card.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
 
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.ZyUrC-qMMLg\\= > div > div > button.OQmO2jtf5sk\\=.MA7FzqzX9xg\\=.eyMkwclB09Q\\=.uvj-8BWgn40\\=._5TIpyud6I34\\=.AaTv-0stjg4\\=.qiLyHE5yesE\\=');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_make_order.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.X3RIOxB4Hfw\\=.hnvkvWHmDgE\\=.JwuYSr-6W7I\\= > div > div:nth-child(4) > div');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_map.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div:nth-child(3) > div.qQAeyTKG3nc\\= > div.BeEM96aK9X8\\=._7AIb7Av-2yo\\=._98OZ2AcB34s\\= > div > div.TS06ZCjWiuw\\= > button:nth-child(1)');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_map_pvz.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
 
     await browser.close();  // Закрытие браузера
 })();
