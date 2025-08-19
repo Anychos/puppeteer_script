b76cde4e38456708da48e6e9a51ce3d8110644ae
Anychnyi_Anton <anychnyianton@gmail.com>
2025-08-19 10:27:59 +0400

    добавлены новые проверки со скринами на другие экраны

---
+++ test_4k_cart_page.js
@@ -0,0 +1,35 @@
+/*
+    Этот скрипт открывает сайт в браузере Chrome с заданным разрешением экрана,
+    переходит в карточку товара по клику
+    и делает скриншот страницы
+ */
+
+const puppeteer = require('puppeteer');  // Импорт Puppeteer
+
+(async () => {  // Функция инициализации браузера
+    const browser = await puppeteer.launch({
+        headless: false,  // Запуск браузера с UI
+        defaultViewport: {  // Кастомные настройки разрешения экрана
+            width: 2160,
+            height: 3840,
+            deviceScaleFactor: 2  // Pixel Ratio
+        },
+        args: ['--window-size=2160,3840']
+    });
+
+    const page = await browser.newPage();  // Запуск браузера
+    await page.goto('https://a101-shop-front-specstorea-355.ktsdev.ru/?platform=stand', { waitUntil: 'networkidle2' });  // Открытие сайта с параметром ожидания
+
+    await new Promise(resolve => setTimeout(resolve, 5000));  // Ожидание 3 секунды
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.X3RIOxB4Hfw\\=.hnvkvWHmDgE\\= > div.bElX2h-X9oE\\= > button');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_product_cart.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await browser.close();  // Закрытие браузера
+})();
--- test_4k_item_page.js
+++ test_4k_item_page.js
@@ -18,11 +18,38 @@ const puppeteer = require('puppeteer');  // Импорт Puppeteer
     });
 
     const page = await browser.newPage();  // Запуск браузера
-    await page.goto('https://a101-shop-front-specstorea-355.ktsdev.ru/?platform=stand', { waitUntil: 'networkidle2' });  // Открытие сайта с параметром ожидания
+    await page.goto('https://a101-shop-front-specstorea-359.ktsdev.ru/?platform=stand', { waitUntil: 'networkidle2' });  // Открытие сайта с параметром ожидания
 
     await new Promise(resolve => setTimeout(resolve, 5000));  // Ожидание 3 секунды
     // Скриншот
     await page.screenshot({ path: 'delayed-screenshot_catalog.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.X3RIOxB4Hfw\\=.hnvkvWHmDgE\\= > div.bElX2h-X9oE\\= > div > button:nth-child(1)');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_sort_menu.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div:nth-child(3) > div > div.GhxakRDroQA\\=.-WfC5FmobkM\\=.NUJlXB8V3vE\\= > button');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // Клик по полю ввода. Используется CSS селектор
+    await page.click('#root > div.eRdLFkQ6t3M\\= > div > div > div.X3RIOxB4Hfw\\=.hnvkvWHmDgE\\= > div.bElX2h-X9oE\\= > div > button:nth-child(2)');
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Скриншот
+    await page.screenshot({ path: 'delayed-screenshot_filter_menu.png', fullPage: true });  // path для каждого скриншота нужно менять имя, иначе не будет сохранять
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await page.click('#root > div:nth-child(3) > div > div.GhxakRDroQA\\=.-WfC5FmobkM\\=.GCNljVv-K98\\= > button');
 
     await new Promise(resolve => setTimeout(resolve, 5000));
 
